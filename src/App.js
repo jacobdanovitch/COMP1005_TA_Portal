7c4398989399a564e10a2d7b6fe266f00f890f74
Jacob Danovitch <jacobdanovitch@cmail.carleton.ca>
2018-11-21 15:44:17 -0500

    react+electron+flask

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React + Electron = <span role="img" aria-label="love">😍</span></h2>
+          <h2>React + Electron = <span role="img" aria-label="love">😍!</span></h2>
         </div>
         <p className="App-intro">
           <b> Release 0.2.7 </b>
