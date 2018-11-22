582d992245f07176b26afb52ba8f82c9980a6387
Jacob Danovitch <jacobdanovitch@cmail.carleton.ca>
2018-11-21 21:09:46 -0500

    remove unnecessary stuff

--- src/Home/UploadForm/UploadForm.js
+++ src/Home/UploadForm/UploadForm.js
@@ -1,54 +1,6 @@
 import React from 'react'
-import axios, {post} from 'axios';
 
 class UploadForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            file: null
-        }
-    }
-
-    onFormSubmit = (e) => {
-        e.preventDefault(); // Stop form submit
-        this
-            .fileUpload(this.state.file)
-            .then((response) => {
-                console.log(response.data);
-            })
-    };
-
-    onChange = (e) => {
-        this.setState({file: e.target.files[0]})
-    };
-
-    fileUpload = (file) => {
-        const url = "/upload";
-        const formData = new FormData();
-        formData.append('file', file);
-        const config = {
-            headers: {
-                'content-type': 'multipart/form-data'
-            }
-        };
-
-        return post(url, formData, config)
-    };
-
-    test = () => {
-        const url = "/test";
-        const formData = new FormData();
-        formData.append('data', "hello");
-
-        const config = {
-            headers: {
-                'content-type': 'application/json' // 'multipart/form-data'
-            }
-        };
-
-        return post(url, formData)
-    };
-
     render() {
         return (
             <form method="post" encType="multipart/form-data" action="/upload">
